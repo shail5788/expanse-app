2cd92b266b2955387f1f82d92b7f6771829546c6
shailendra <shail5788@gmail.com>
2019-09-10 21:02:47 +0530

    add dashboard router module

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DashboardComponentComponent } from "./dashboard-component/dashboard-component.component";
+import { DashboardRoutingModule } from "./dashboard.route.module";
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [DashboardComponentComponent],
+  imports: [CommonModule, DashboardRoutingModule]
 })
-export class DashboardModule { }
+export class DashboardModule {}
