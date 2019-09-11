994dc1211b979ed36c991c97af8ceae8c1ad01f7
shailendra <shail5788@gmail.com>
2019-09-11 21:36:23 +0530

    implement theme in angular1

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-// import { ChartsModule } from "ng2-charts";
+import { ChartsModule } from "ng2-charts";
 import { DashboardComponentComponent } from "./dashboard-component/dashboard-component.component";
 import { DashboardRoutingModule } from "./dashboard.route.module";
 import { GradiantCardComponent } from "./gradiant-card/gradiant-card.component";
@@ -11,6 +11,6 @@ import { BarGraphComponent } from "./bar-graph/bar-graph.component";
     GradiantCardComponent,
     BarGraphComponent
   ],
-  imports: [CommonModule, DashboardRoutingModule]
+  imports: [CommonModule, DashboardRoutingModule, ChartsModule]
 })
 export class DashboardModule {}
