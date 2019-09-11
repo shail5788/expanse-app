b352b835c64b1777ed5a99f54afbc6eb471656a2
shailendra <shail5788@gmail.com>
2019-09-11 21:27:32 +0530

    implement theme in angular

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,10 +4,10 @@ import { HeaderComponent } from "./header/header.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { FlexLayoutModule } from "@angular/flex-layout";
-
+import { RouterModule, Routes } from "@angular/router";
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent, FooterComponent],
-  imports: [CommonModule, FlexLayoutModule],
+  imports: [CommonModule, FlexLayoutModule, RouterModule],
   exports: [FooterComponent, HeaderComponent, SidebarComponent]
 })
 export class LayoutModule {}
